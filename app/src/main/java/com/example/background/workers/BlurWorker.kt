2d96659eb2ba216d2222c3171a6018325221ad2e
Ivan Kaptue <ivanokaptue@gmail.com>
2022-04-24 16:48:19 -0400

    Add input and output

--- app/src/main/java/com/example/background/workers/BlurWorker.kt
+++ app/src/main/java/com/example/background/workers/BlurWorker.kt
@@ -2,10 +2,12 @@ package com.example.background.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.example.background.R
+import androidx.work.workDataOf
+import com.example.background.KEY_IMAGE_URI
 
 private const val TAG = "BlurWorker"
 
@@ -14,13 +16,25 @@ class BlurWorker(context: Context, params: WorkerParameters) :
 
     override fun doWork(): Result {
         val appContext = applicationContext
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
 
         makeStatusNotification("Blurring image", appContext)
 
         return try {
-            val picture = BitmapFactory.decodeResource(
-                appContext.resources,
-                R.drawable.android_cupcake
+//            val picture = BitmapFactory.decodeResource(
+//                appContext.resources,
+//                R.drawable.android_cupcake
+//            )
+
+            if (resourceUri.isNullOrEmpty()) {
+                Log.e(TAG, "Invalid input uri")
+                throw IllegalArgumentException("Invalid input uri")
+            }
+
+            val resolver = appContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri))
             )
 
             val output = blurBitmap(picture, appContext)
@@ -30,7 +44,9 @@ class BlurWorker(context: Context, params: WorkerParameters) :
 
             makeStatusNotification("Output is $outputUri", appContext)
 
-            Result.success()
+            val outputData = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+            Result.success(outputData)
         } catch (throwable: Throwable) {
             Log.e(TAG, "Error applying blur")
             Result.failure()
